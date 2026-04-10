fb262007372195d9b4510a3cdc351105ffec746f
vikashbond <vikash.mishra22110@gmail.com>
2026-04-10 15:40:55 +0000

    Initial clean commit - removed terraform cache

---
+++ terraform/terraform-lab/project1/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "rg-terraform-lab"
+location = "East US"
---
+++ terraform/terraform-lab/project1/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+   description = "This is resource group name"
+   type = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type = string
+}
